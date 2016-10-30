75f0a321f70adf6ddd4b43619922f88c9b1f5701
jpfuruya <jpfuruya@gmail.com>
2016-10-30 15:19:13 +0800

    added a form to add data to database

---
+++ exercise5/add_data.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php
+include_once 'dbconfig.php';
+if(isset($_POST['btn-save']))
+{
+ // variables for input data
+ $name = $_POST['name'];
+ $nickname = $_POST['lastname'];
+ $email = $_POST['email'];
+ $hadd = $_POST['hadd'];
+ $gender = $_POST['gender']; 
+ $cpnum = $_POST['cpnum'];
+ $comment = $_POST['comment']; 
+ // sql query for inserting data into database
+ 
+        $sql_query = "INSERT INTO users(name,nickname,email,hadd,gender,cpnum,comment) VALUES('$name','$nickname','$email','$hadd','$gender','$cpnum','$comment')";
+ mysql_query($sql_query);
+        
+        // sql query for inserting data into database
+ 
+}
+?>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Form</title>
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+<center>
+
+<div id="header">
+ <div id="content">
+    <label>Please fill out the form.</label>
+    </div>
+</div>
+<div id="body">
+
+<p id="form">Please enter the following: </p>
+	
+<?php
+		// define variables and set to empty values
+		$nameErr = $nicknameErr = $emailErr = $genderErr =$cpnumErr = "";
+		$name = $nickname = $email = $hadd = $gender = $cpnum = $comment = "";
+		
+		
+		
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		  	if (empty($_POST["name"])) {
+				$nameErr = "Name is required";
+		  	} else {
+				$name = test_input($_POST["name"]);
+				// check if name only contains letters and whitespace
+				if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+					$nameErr = "Only letters and white space allowed";
+					$name = "";
+				}
+		  	}
+
+			
+			
+			if(empty($_POST["nickname"])){
+		  		$nicknameErr = "Nickname is required";
+			}else{
+		  		$nickname = test_input($_POST["nickname"]);
+		  		if (!preg_match("/^[a-zA-Z ]*$/",$nickname)) {
+				$nicknameErr = "Only letters and white space allowed";
+				$nickname = "";
+				}
+			}
+		  
+		  
+		  
+			if (empty($_POST["email"])) {
+			$emailErr = "Email is required";
+		  	} else {
+				$email = test_input($_POST["email"]);
+				// check if e-mail address is well-formed
+				if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+			  	$emailErr = "Invalid email format";
+			  	$email = "";
+				}
+		  	}
+
+			
+			
+			if(empty($_POST["hadd"])){
+			  	$hadd = "";
+			}else{
+			  	$hadd = test_input($_POST["hadd"]);
+			}
+
+			
+
+		  	if (empty($_POST["gender"])) {
+				$genderErr = "Gender is required";
+		  	} else {
+				$gender = test_input($_POST["gender"]);
+		  	}
+			
+			
+
+			if (empty($_POST["cpnum"])) {
+				$cpnumErr = "Mobile number is required";
+		  	} else {
+				$cpnum = test_input($_POST["cpnum"]);
+				if(!preg_match("/^[0-9]*$/",$cpnum)){
+					$cpnumErr = "Only numbers are allowed";
+					$cpnum = "";
+				}
+		  	}
+			
+			
+			
+			if (empty($_POST["comment"])) {
+				$comment = "";
+		  	} else {
+				$comment = test_input($_POST["comment"]);
+		  	}	
+		}
+
+		
+	function test_input($data) {
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+			}
+	?>
+	
+	
+			<p><span class="error">* required field.</span></p>
+			<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+		
+ <div id="content">
+    <form method="post">
+    <table align="center">
+    <tr>
+    <td align="center"><a href="mypage.php">back to main page</a></td>
+    </tr>
+    <tr>
+    <td><input type="text" name="name" placeholder="Name" value="<?php echo $name;?>">
+			<span class="error">* <?php echo $namErr;?></span>
+			<br><br> </td>
+    </tr>
+    <tr>
+    <td><input type="text" name="nickname" placeholder="Nickkame" value="<?php echo $nickname;?>">
+			<span class="error">* <?php echo $nicknamErr;?></span></td>
+    </tr>
+    <tr>
+    <td><input type="text" name="email" placeholder="Email" value="<?php echo $email;?>">
+			<span class="error">* <?php echo $emailErr;?></span></td>
+    </tr>
+	<tr>
+    <td><textarea name="hadd" placeholder ="hadd" rows ="2" cols="30"><?php echo $hadd;?></textarea>></span></td>
+    </tr>
+
+	<tr>
+    <td><input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+			<input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+			<span class="error">* <?php echo $genderErr;?></span>
+    </tr>
+
+	<tr>
+    <td><input type="text" name="cpnum" placeholder="Cellphone Number" value="<?php echo $cpnum;?>">
+			<span class="error">* <?php echo $cpnumErr;?></span></td>
+    </tr>
+
+	<tr>
+    <td><textarea name="comment" placeholder="Comment" rows="5" cols="40"><?php echo $comment;?></textarea></span></td>
+    </tr>
+	
+    <tr>
+    <td><button type="submit" name="btn-save"><strong>SAVE</strong></button></td>
+    </tr>
+    </table>
+    </form>
+    </div>
+</div>
+
+</center>
+</body>
