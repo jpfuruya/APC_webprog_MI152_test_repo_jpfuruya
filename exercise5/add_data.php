41a969966c42c2effa36db34af4e4a576713b0c8
jpfuruya <jpfuruya@gmail.com>
2016-11-01 21:55:57 +0800

    commited the wrong add_data.php in my last commit.

--- exercise5/add_data.php
+++ exercise5/add_data.php
@@ -1,3 +1,94 @@
+<html>
+<head>
+<title>My Site</title>
+<style>
+
+body{
+background-image: url("t.jpg");
+background-size:cover;
+background-repeat:no-repeat;
+margin:20px 20px 20px 20px;
+padding: 20px 30px 20px 20px;
+border:dashed;
+}
+
+h1{
+font-family:impact;
+font-size:40px;
+align:center;
+}
+
+
+
+#form{
+font-family:comic sans ms;
+font-size:28px;
+text-align:center;
+}
+
+#eto{
+	width: 1000px;
+	height: 1200px;
+	background: rgba(0,0,0,0.1);
+	padding: 5px;
+	color: black;
+	margin: auto;
+	margin-top: 5px;
+	margin-bottom: 5px;
+	font-family:comic sans ms;
+	font-size:25px;
+	vertical-align:baseline;
+	}
+
+#hey {
+	vertical-align:baseline;
+	padding-left:80px;
+	font-family:comic sans ms;
+	font-size:25px;
+	text-align:center;
+	}
+</style>
+<link rel="stylesheet" href="style.css" type="text/css" />
+	</head>
+	<body>
+
+	<img src = "log.png" width ="150" height "150"/>
+	
+	
+	<a href="form.php" target="_self"  >
+	<img src="form.png" width ="80" height ="40"align ="right"/>
+	</a> 
+	
+	<a href="trivia.php" target="_self" align >
+	<img src="trivia.png" width ="80" height ="40"align ="right"/>
+	</a> 
+
+	<a href="works.php" target="_self" align >
+	<img src="works.png" width ="80" height ="40"align ="right"/>
+	</a> 
+
+	<a href="sites.php" target="_self" align >
+	<img src="sites.png" width ="80" height ="40"align ="right"/>
+	</a>
+
+	<a href="about.php" target="_self" align >
+	<img src="about.png" width ="80" height ="40"align ="right"/>
+	</a>
+
+	<a href="mypage.php" target="_self" align >
+	<img src="home.png" width ="80" height ="40"align ="right"/>
+	</a> 
+
+
+
+	</br>
+</br>
+</br>
+</br>
+<div id="eto">
+<center>
+<h1>Form</h1>
+
 <?php
 include_once 'dbconfig.php';
 if(isset($_POST['btn-save']))
