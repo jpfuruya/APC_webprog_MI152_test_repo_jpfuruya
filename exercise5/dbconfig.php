4d0e16fd478cbc03db7b040e23b4ff214bbd73bb
John Rafael Furuya <jpfuruya@gmail.com>
2016-11-03 09:28:01 +0800

    updated mysql to mysqli in dbconfig.php.

--- exercise5/dbconfig.php
+++ exercise5/dbconfig.php
@@ -3,6 +3,6 @@ $host = "localhost";
 $user = "root";
 $password = "";
 $datbase = "form";
-mysql_connect($host,$user,$password);
-mysql_select_db($datbase);
+$link = mysqli_connect($host,$user,$password);
+mysqli_select_db($link, $datbase);
 ?>
