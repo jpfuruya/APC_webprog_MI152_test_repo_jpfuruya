08c6d935db55a5810bd31be6583adae1256bc91c
John Rafael Furuya <jpfuruya@gmail.com>
2016-11-03 09:33:20 +0800

    updated mysql to mysqli in edit_data.php.

--- exercise5/edit_data.php
+++ exercise5/edit_data.php
@@ -1,4 +1,4 @@
-<<html>
+<<<html>
 <head>
 <title>My Site</title>
 <style>
@@ -56,8 +56,8 @@ include_once 'dbconfig.php';
 if(isset($_GET['edit_id']))
 {
  $sql_query="SELECT * FROM users WHERE user_id=".$_GET['edit_id'];
- $result_set=mysql_query($sql_query);
- $fetched_row=mysql_fetch_array($result_set);
+ $result_set=mysqli_query($link, $sql_query);
+ $fetched_row=mysqli_fetch_array($result_set);
 }
 if(isset($_POST['btn-update']))
 {
@@ -76,7 +76,7 @@ if(isset($_POST['btn-update']))
  // sql query for update data into database 
  
 // sql query execution function
- if(mysql_query($sql_query))
+ if(mysqli_query($sql_query))
  {
   ?>
   <script type="text/javascript">
