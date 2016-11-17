1f17bf4f96ee5a6c7792e207ac6fd5a9f5aa8f30
John Rafael Furuya <jpfuruya@gmail.com>
2016-11-17 10:49:00 +0800

    major changes in design were made in user_add.php

--- exercise6/codeigniter/application/views/user_add.php
+++ exercise6/codeigniter/application/views/user_add.php
@@ -27,8 +27,8 @@ font-size:30px;
 }
 
 #prof{
-   width: 450px;
-   height: 560px;
+   width: 900px;
+   height: 960px;
    background: rgba(0,0,0,0.1);
    padding: 5px;
    color: black;
@@ -70,51 +70,120 @@ font-size:30px;
 <br>
 <br>
 <br>
-         <?php 
-            echo form_open('User_controller/add_user');
 
+<center>
+<table> 
+         <?php 
+
+
+            echo form_open('User_controller/add_user');
+echo "<tr>"; 
+echo "<td>"; 
             echo form_label('User Number:'); 
             echo form_input(array('id'=>'user_id','name'=>'user_id')); 
             echo "<br/>"; 
+            echo "</tr>"; 
+            echo "</td>"; 
 
+
+echo "<tr>"; 
+echo "<td>"; 
             echo form_label('Name:'); 
             echo form_input(array('id'=>'name','name'=>'name')); 
             echo "<br/>"; 
+echo "</tr>"; 
+echo "</td>"; 
 
+
+echo "<tr>"; 
+echo "<td>"; 
             echo form_label('Nickname:'); 
             echo form_input(array('id'=>'nickname','name'=>'nickname')); 
             echo "<br/>";
+echo "</tr>"; 
+echo "</td>"; 
 
+
+echo "<tr>"; 
+echo "<td>";  
             
             echo form_label('Email:'); 
             echo form_input(array('id'=>'email','name'=>'email')); 
             echo "<br/>";
 
+echo "</tr>"; 
+echo "</td>"; 
             
+
+
+echo "<tr>"; 
+echo "<td>";  
+
             echo form_label('Home Address:'); 
             echo form_input(array('id'=>'hadd','name'=>'hadd')); 
             echo "<br/>";
 
+echo "</tr>"; 
+echo "</td>"; 
             
+
+
+
+
+echo "<tr>"; 
+echo "<td>";  
+          
             echo form_label('gender:'); 
             echo form_input(array('id'=>'gender','name'=>'gender')); 
             echo "<br/>";
 
-           
+echo "</tr>"; 
+echo "</td>";            
+
+echo "<tr>"; 
+echo "<td>";  
+          
+
             echo form_label('Cellphone Number:'); 
             echo form_input(array('id'=>'cpnum','name'=>'cpnum')); 
             echo "<br/>";
 
+
+echo "</tr>"; 
+echo "</td>";  
+
             
+echo "<tr>"; 
+echo "<td>";  
+
             echo form_label('Comment:'); 
             echo form_input(array('id'=>'comment','name'=>'comment')); 
             echo "<br/>";
 
 
+
+echo "</tr>"; 
+echo "</td>";  
+
+
+echo "<tr>"; 
+echo "<td>";  
 			
-            echo form_submit(array('id'=>'submit','value'=>'Add')); 
+            echo form_submit(array('id'=>'submit','value'=>'CLICK TO ADD DATA')); 
             echo form_close(); 
+
+
+
+            echo "</tr>"; 
+echo "</td>";  
+
          ?> 
+
          </div>
+
+
+
+         </table>
+         </center>
    </body>
 </html>
