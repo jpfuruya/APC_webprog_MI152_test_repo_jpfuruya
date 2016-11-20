1218890840a942a03ceae021f040f1d04264154f
jpfuruya <jpfuruya@gmail.com>
2016-11-20 18:05:22 +0800

    major changes in design of user_edit.php

--- exercise6/codeigniter/application/views/user_edit.php
+++ exercise6/codeigniter/application/views/user_edit.php
@@ -65,51 +65,98 @@ font-size:30px;
 <br>
 <br>
 <br>
-
+<table>
       <form method = "" action = "">
 		
          <?php 
             echo form_open('user_controller/update_user'); 
             echo form_hidden('old_user_id',$old_user_id); 
 
+			
+			echo "<tr>"; 
+			echo "<td>"; 
+			
             echo form_label('user_id'); 
             echo form_input(array('id'=>'user_id',
                'name'=>'user_id','value'=>$records[0]->user_id)); 
             echo "<br/>"; 
-				
+			echo "</tr>"; 
+echo "</td>"; 
+			
+			
+			
+			
+			echo "<tr>"; 
+			echo "<td>"; 	
             echo form_label('nickname'); 
             echo form_input(array('id'=>'nickname','name'=>'nickname',
                'value'=>$records[0]->name)); 
             echo "<br/>"; 
-
+echo "</tr>"; 
+echo "</td>"; 
+			
+			
+			echo "<tr>"; 
+			echo "<td>"; 	
             echo form_label('email'); 
             echo form_input(array('id'=>'email','name'=>'email',
                'value'=>$records[0]->name)); 
             echo "<br/>"; 
-
+			echo "</tr>"; 
+echo "</td>"; 
+			
+			
+			
+			echo "<tr>"; 
+			echo "<td>"; 	            
             echo form_label('hadd'); 
             echo form_input(array('id'=>'hadd','name'=>'hadd',
                'value'=>$records[0]->name)); 
             echo "<br/>"; 
+echo "</tr>"; 
+echo "</td>"; 
 
+			
+			echo "<tr>"; 
+			echo "<td>"; 	 
             echo form_label('gender'); 
             echo form_input(array('id'=>'gender','name'=>'gender',
                'value'=>$records[0]->name)); 
             echo "<br/>"; 
-
+echo "</tr>"; 
+echo "</td>"; 
+			
+			
+			echo "<tr>"; 
+			echo "<td>"; 	
             echo form_label('cpnum'); 
             echo form_input(array('id'=>'cpnum','name'=>'cpnum',
                'value'=>$records[0]->name)); 
             echo "<br/>"; 
-
+echo "</tr>"; 
+echo "</td>"; 
+			
+			
+			echo "<tr>"; 
+			echo "<td>"; 	      
             echo form_label('comment'); 
             echo form_input(array('id'=>'comment','name'=>'comment',
                'value'=>$records[0]->name)); 
             echo "<br/>"; 
-				
+			echo "</tr>"; 
+echo "</td>"; 
+	
+	
+	echo "<tr>"; 
+			echo "<td>";
             echo form_submit(array('id'=>'submit','value'=>'Edit')); 
             echo form_close();
-         ?> 
+echo "</tr>"; 
+echo "</td>"; 
+
+			?> 
+			
+			</table>
 			
       </form> 
       </div>
