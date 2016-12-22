2de6de7498f03a6443cbc8059a616cc57dce6739
jpfuruya <jpfuruya@gmail.com>
2016-12-22 23:03:14 +0800

    Added some improvement on login page.

--- exercise7/yii/basic/views/site/login.php
+++ exercise7/yii/basic/views/site/login.php
@@ -42,6 +42,9 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <div class="col-lg-offset-1" style="color:#999;">
         You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
-        To modify the username/password, please check out the code <code>app\models\User::$users</code>.
+        To modify the username/password, please check out the code 
+<br>
+        <img src = "log.png" width="200" height="80" >
+    <br>
     </div>
 </div>
