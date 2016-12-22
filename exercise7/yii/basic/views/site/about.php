e55b6b2a319c22e3ae83a4c5cbae1c5d23331e08
jpfuruya <jpfuruya@gmail.com>
2016-12-22 22:39:17 +0800

    Added some improvement on my about page.

--- exercise7/yii/basic/views/site/about.php
+++ exercise7/yii/basic/views/site/about.php
@@ -10,9 +10,38 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p>
+<p>
+    	Hey There I'm Rafael a college student currently taking BS in Information </br>
+		Technology major in mobile and internet. I'm half Japanese half Filipino </br>
+		born and raised here in the Philippines. My friends and block mates used </br>
+		to call me Raf and Furuys, but you guys can call me anything you want.</br>
+		Both of my parents are in Japan and I'm only with my sister at home whose</br>
+		also a college student like me. So yeah basically me and my sister are </br>
+		kinda Independent , cause we have no choice and we need to, in order to </br>
+		live. Another thing we don't have yaya's or kasambahay at home. 
+		
+
+		
+		<br>
+	
+
+		<h3>Hobbies and interest:</h3>
+		
+		Hobbies uhhm it's kinda weird because aside for programming what I did </br>
+		when I have a free time is usually spending my time in social Media </br>
+		like Twitter, but not facebook haha I do hate facebook. Blogging in my </br>
+		Tumblr account has also became my habby. Lately , I'm currently in to </br>
+		Photo editing through the use of Photoshop, It started with my design </br>
+		subject such as artapre and GUI programming and after a while it </br>
+		became a hobby of mine , weird is'nt it? </br>
+		Aside from that, I'm also interested on handheld console especially </br>
+		devices that were made by Nintendo. I'm a big fan of Super Mario and </br>
+		Pokemon games. I like travelling especially with my friends. I feel </br>
+		recharged and free when I'm with them. Occasionally read books when I</br>
+		Do feel like reading one.</br>That's it , I just want to keep it simple and short.
+		</br>
+		Hope you Have a wonderful time reading!
+		</p>
 
     <code><?= __FILE__ ?></code>
 </div>
