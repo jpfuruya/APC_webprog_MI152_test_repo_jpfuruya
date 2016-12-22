8a595b5f0254c556717ab9a3834c5d1b11041582
jpfuruya <jpfuruya@gmail.com>
2016-12-22 22:26:54 +0800

    added some link on my homepage.

--- exercise7/yii/basic/views/site/index.php
+++ exercise7/yii/basic/views/site/index.php
@@ -25,19 +25,19 @@ $this->title = 'My Yii Application';
 
                 <p>Hi My name is  Rafael, nice to meet you!</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Twitter &raquo;</a></p>
+                <p><a class="btn btn-default" href="https://twitter.com/Furuyarafael">Twitter &raquo;</a></p>
             </div>
             <div class="col-lg-4">
                 <h2>Hey Their!</h2>
                 <p>Hi My name is  Rafael, nice to meet you!</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Instagram &raquo;</a></p>
+                <p><a class="btn btn-default" href="https://www.instagram.com/rafaelfuruya">Instagram &raquo;</a></p>
             </div>
             <div class="col-lg-4">
                 <h2>Hey Their!</h2>
 
                 <p>Hi My name is  Rafael, nice to meet you!</p>
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Facebook &raquo;</a></p>
+                <p><a class="btn btn-default" href="https://www.facebook.com/jpfuruya">Facebook &raquo;</a></p>
             </div>
         </div>
 
